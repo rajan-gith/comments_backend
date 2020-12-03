ae3f44ad7ef48ab67de9ac1b40b85d73d8a5bec9
rajan-gith <rajan.beryls@gmail.com>
2020-12-03 16:49:35 +0530

    comments scaffold

---
+++ db/migrate/20201203103438_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.string :title
+      t.string :description
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
